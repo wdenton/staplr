333788f29bbc7e098c5c079917716097cc49746a
William Denton <wtd@pobox.com>
2018-08-31 12:28:00 -0400

    Refactored so it can run cleanly from cron

--- setup-staplr.sh
+++ setup-staplr.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set up and configure everything necessary for
 # Sonic Pi to run headless (on wdenton.uit.yorku.ca)
@@ -13,39 +13,85 @@ COMPOSITION=$1
 
 cd ~/src/staplr/ || exit
 
-# Is kernel module snd_aloop enabled?
-lsmod | grep -q snd_aloop
-if [ "$?" -ne 0 ] ; then
-    echo "snd_aloop module must be enabled ... enabling"
+##
+## Kernel module snd_aloop
+##
+
+# This needs to be enabled.  Once that's done, it won't go away,
+# but it needs to be checked.
+
+if ! lsmod | grep -q snd_aloop
+then
+    echo "snd_aloop module not enabled ... enabling"
     sudo modprobe snd-aloop
 fi
 
 tmux start-server
 
-# tmux has-session -t staplr 2>/dev/null
-# if [ "$?" -eq 1 ] ; then
-#     echo "Session staplr exists; killing it"
-#     echo "You may need to kill Sonic Pi manually and rerun everything"
-#     tmux kill-session -t staplr
-# fi
+# If a STAPLR tmux session is already running, then we want to kill
+# it. But we can't just kill each window, because that will leaves
+# daemons and other processes running. We need to hit Ctrl-c in each
+# window to stop whatever is running, and then we can kill the window.
+#
+# Worst comes to worst, run
+# $ tmux attach -t staplr
+# and kill everything by hand.
 
-echo "Killing session staplr ..."
-tmux kill-session -t staplr
+# if [ "$?" -eq 0 ] ; then
+# tmux has-session -t staplr 2>/dev/null
+if tmux has-session -t staplr
+then
+    # By the way, I don't know why a ! isn't needed there, since it
+    # returns 0 if the session exists.  This reads cleanly, but
+    # it doesn't seem to work the Unix way.
+    echo "Session staplr exists; killing it ..."
+    tmux send-keys -t staplr:4 "C-c"
+    tmux kill-window -t staplr:4
+    sleep 1
+    tmux send-keys -t staplr:3 "C-c"
+    tmux kill-window -t staplr:3
+    sleep 1
+    tmux send-keys -t staplr:2 "C-c"
+    tmux kill-window -t staplr:2
+    sleep 1
+    tmux send-keys -t staplr:1 "C-c"
+    tmux kill-window -t staplr:1
+    sleep 1
+fi
 
 echo "Making new session staplr ..."
 tmux new-session -d -s "staplr"
 
+##
+## JACK
+##
+
 echo "JACK ..."
 tmux new-window -t staplr:2 -n "jack"
-# tmux send-keys -t staplr:2 "cd ~/jack2/build/linux/; ./jackd -dalsa -dhw:0 -r44100 -p1024 -n2" "C-m"
-tmux send-keys -t staplr:2 "jackd -dalsa -dhw:0 -r44100 -p1024 -n2" "C-m"
+# Use dbus-launch so it can be run without any X display variables being set,
+# which means it can be called from cron.
+# Otherwise, you get this error: "Unable to autolaunch a dbus-daemon without a $DISPLAY for X11."
+# If X variables are set (i.e. you sshed in from an X session) it doesn't hurt anything.
+tmux send-keys -t staplr:2 "dbus-launch jackd -dalsa -dhw:0 -r44100 -p1024 -n2" "C-m"
 sleep 2
 
-echo "Sonic Pi ... (then sleeping 20)"
+# On 14.04 I had to run this, because I had the new jackd in my home dir:
+# tmux send-keys -t staplr:2 "cd ~/jack2/build/linux/; ./jackd -dalsa -dhw:0 -r44100 -p1024 -n2" "C-m"
+# ^^ Delete when I'm sure everything is working.
+
+##
+## Sonic Pi
+##
+
+echo "Sonic Pi ... (then sleeping)"
 tmux new-window -t staplr:3 -n "sonicpi"
 # tmux send-keys -t staplr:3 "xvfb-run /usr/local/src/sonic-pi-2.6.0/bin/sonic-pi" "C-m"
 tmux send-keys -t staplr:3 "xvfb-run sonic-pi" "C-m"
-sleep 20
+sleep 10
+
+##
+## DarkIce
+##
 
 echo "DarkIce ..."
 tmux new-window -t staplr:4 -n "darkice"
@@ -55,9 +101,21 @@ sleep 2
 echo "Connecting SuperCollider and Darkice ..."
 tmux send-keys -t staplr:1 "./connect-darkice.sh" "C-m"
 
-echo "Starting composition $COMPOSITION ..."
+##
+## Play the composition
+##
+
+echo "Playing $COMPOSITION ..."
 tmux send-keys -t staplr:1 "cat $COMPOSITION | sonic_pi" "C-m"
+
+##
+## Leave things tidy
+##
 
 echo "Tailing log ..."
 tmux send-keys -t staplr:1 "tail -f ~/.sonic-pi/log/debug.log" "C-m"
 tmux select-window -t 1
+
+# Now everything is left so that
+# $ tmux attach -t staplr
+# show the log tail.

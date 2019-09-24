6db5eb299793fb1a1d16c730b9ce0a20974b2a4a
William Denton <wtd@pobox.com>
2019-09-24 18:46:47 -0400

    Tidy up where data is

--- fake-the-data.sh
+++ fake-the-data.sh
@@ -3,6 +3,6 @@
 for I in `seq -w 1 60`
 do
     echo -n "$I\r"
-    cp test-data/structured/S-${I}.json staplr-branch-activity-01.json
+    cp data/test/structured/S-${I}.json data/live/activity.json
     sleep 60
 done
